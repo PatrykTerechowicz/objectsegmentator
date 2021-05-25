8935a9333d702f63293ee0688fedc18b0fda7d54
Patryk Terechowicz <terechsama@gmail.com>
2021-05-25 11:10:19 +0200

    Fixed plotting

--- plotting.py
+++ plotting.py
@@ -5,12 +5,13 @@ from torch import Tensor
 from torchvision.utils import make_grid, save_image
 
 def plot_history(history):
-    fig, axs = plt.subplots(2, 2)
+    fig, axs = plt.subplots(4, 1)
+    fig.tight_layout()
     for n, key in enumerate(history):
         data = history[key]
         ax = axs[n]
         ax.plot(data)
-        ax.title(key)
+        ax.set_title(key)
     return fig
 
 def save_batch(batch: Tensor, fp: str):
