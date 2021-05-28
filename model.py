e923d35adc5f3ded45e4eacab6c48ffb5202a23c
Patryk Terechowicz <terechsama@gmail.com>
2021-05-28 10:47:20 +0200

    Changed architecture: RELU6 to Hardswish

--- model.py
+++ model.py
@@ -35,7 +35,7 @@ class ConvBNActivation(nn.Sequential):
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         if activation_layer is None:
-            activation_layer = nn.ReLU6
+            activation_layer = nn.Hardswish
         super(ConvBNActivation, self).__init__(
             nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, dilation=dilation, groups=groups,
                       bias=False),
@@ -59,7 +59,7 @@ class TransposeConvBNActivation(nn.Sequential):
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         if activation_layer is None:
-            activation_layer = nn.ReLU6
+            activation_layer = nn.Hardswish
         super(TransposeConvBNActivation, self).__init__(
             nn.ConvTranspose2d(in_planes, out_planes, kernel_size, stride, padding=padding, dilation=dilation, groups=groups),
             norm_layer(out_planes),
@@ -112,7 +112,9 @@ class Segmentator(nn.Module):
         self.up_sampler0 = TransposedInvertedResidual(64, 64, 6)
         self.up_sampler1 = TransposedInvertedResidual(112, 64, 6)
         self.up_sampler2 = TransposedInvertedResidual(96, 32, 6)
-        self.last_conv = InvertedResidual(48, 1, 1, 1)
+        self.conv0 = InvertedResidual(48, 32, 1, 6)
+        self.conv1 = InvertedResidual(32, 16, 1, 6)
+        self.conv2 = InvertedResidual(16, 1, 1, 6)
         self.sig = nn.Sigmoid()
     
     def forward(self, input: Tensor):
@@ -126,7 +128,9 @@ class Segmentator(nn.Module):
         y2_x1 = torch.cat([y2, x1], dim=1)
         y1 = self.up_sampler2(y2_x1)
         y1_x0 = torch.cat([y1, x0], dim=1)
-        y0 = self.last_conv(y1_x0)
+        y0 = self.conv0(y1_x0)
+        y0 = self.conv1(y0)
+        y0 = self.conv2(y0)
         return self.sig(y0)
 
 
@@ -154,12 +158,14 @@ class Segmentator(nn.Module):
         valid_iou /= n
         return valid_loss.cpu().item(), valid_iou.cpu().item()
     
-    def _train_epoch(self, train_loader: data.DataLoader, epoch_idx: int, optimizer: torch.optim.Optimizer):
+    def _train_epoch(self, train_loader: data.DataLoader, epoch_idx: int, optimizer: torch.optim.Optimizer, transform_data):
         assert self.loss_fn, "Model has no set loss function!"
         train_loss = 0
         train_iou = 0
         for image, true_mask in tqdm(train_loader, desc=f"Training E{epoch_idx+1}"):
             optimizer.zero_grad()
+            if transform_data:
+                image, true_mask = transform_data(image, true_mask)
             image = image.cuda()
             true_mask = true_mask.cuda()
             loss, iou = self.calculate_loss_and_metrics(image, true_mask, self.loss_fn, calc_iou)
@@ -172,13 +178,14 @@ class Segmentator(nn.Module):
         train_iou /= n
         return train_loss.cpu().item(), train_iou.cpu().item()
 
-    def generate_images_from_batch(self, images: Tensor, masks: Tensor=None) -> Tensor:
+    def generate_images_from_batch(self, images: Tensor, masks: Tensor=None, denormalizer=None) -> Tensor:
         if not masks is None:
             return put_masks(images, masks)
         masks = self(images)
+        if denormalizer: images = denormalizer(images)
         return put_masks(images, masks)
 
-    def train_and_validate(self, train_loader: data.DataLoader, valid_loader: data.DataLoader, epochs: int, optimizer:torch.optim.Optimizer=torch.optim.Adam):
+    def train_and_validate(self, train_loader: data.DataLoader, valid_loader: data.DataLoader, epochs: int, denormalizer=None, transform_data=None, optimizer:torch.optim.Optimizer=torch.optim.Adam):
         """Train and validates model, if valid_loader is None then won't perform validation.
         """
         history = {"train_loss": [], "train_iou": [], "valid_loss": [], "valid_iou": []}
@@ -188,11 +195,11 @@ class Segmentator(nn.Module):
             self.eval()
             images, true_masks = next(iter(train_loader))
             images, true_masks = images.cuda(), true_masks.cuda()
-            estimate_masked = self.generate_images_from_batch(images).detach().cpu()
+            estimate_masked = self.generate_images_from_batch(images, denormalizer=denormalizer).detach().cpu()
             del images, true_masks
             # TRAINING
             self.train()
-            train_loss, train_iou = self._train_epoch(train_loader, epoch_idx, optimizer)
+            train_loss, train_iou = self._train_epoch(train_loader, epoch_idx, optimizer, transform_data=transform_data)
             # VALIDATE
             # if valid loader not given then we don't perform validation
             if not valid_loader: 
